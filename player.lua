ff71b5462d0c7cce8b3aa90ae27473edb168de34
Jocelyn Gonzalez <jocegonz@gmail.com>
2018-11-03 11:55:35 -0700

    moving player

--- player.lua
+++ player.lua
@@ -1,11 +1,29 @@
+local grid = require 'grid'
+
 local player = {}
 
 function player:load()
-
+	self.pos = 1
 end
 
 function player:update(dt)
-	--
+
+end
+
+function player:keypressed(key)
+	self.pos = grid:moveplayer(key)
+
+		if key == 'up' then
+			return self.pos = self.pos - grid.width
+		if key == 'down' then
+			return self.pos = self.pos + grid.width
+		if key == 'left' then
+			return self.pos = self.pos - 1
+		if key == 'right' then
+			return self.pos = self.pos + 1
+		end
+
+
 end
 
 function player:draw(coords)
