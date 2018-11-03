d93c5c88ecea9448ce7d90d64fb434d248884edf
sophia <sophiabaldonado@gmail.com>
2018-11-03 16:26:19 -0700

    can be revealed;

--- tile.lua
+++ tile.lua
@@ -9,12 +9,11 @@ end
 function Tile:draw(x, y, size)
 	local color = { 255, 150, 0, 255 }
 	if self.isDeadly then color = { 255, 0, 0, 255 } end
+	if self.revealed then color = { 0, 0, 0, 255 } end
 
-	if not self.revealed then
-		love.graphics.setColor(color)
-		love.graphics.rectangle('fill', x, y, size, size)
-		love.graphics.setColor(255, 255, 255, 255)
-	end
+	love.graphics.setColor(color)
+	love.graphics.rectangle('fill', x, y, size, size)
+	love.graphics.setColor(255, 255, 255, 255)
 end
 
 function Tile:setIsDeadly(deadly)
